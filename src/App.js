5770fbb4170e8dd211fb917d57c8b41eacd2cad5
Karol Widuch <rolester92@gmail.com>
2023-07-03 10:09:21 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [newTask, setNewTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        setTasks([...tasks, { text: newTask, done: false }]);
+        setNewTask("");
+    }
+
+    const deleteTask = index => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    }
+
+    const toggleTask = index => {
+        const newTasks = [...tasks];
+        newTasks[index].done = !newTasks[index].done;
+        setTasks(newTasks);
+    }
+
+    return (
+        <div id="app">
+            <div className="new-task-wrapper">
+                <input data-testid="new-task-input" maxLength={50} className="new-task" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Wpisz nowe zadanie"/>
+                <button data-testid="new-task-button" className="add-btn" onClick={addTask}>Dodaj zadanie</button>
+            </div>
+            <ul className="task-list">
+                {tasks.map((task, index) => (
+                    <li key={index} className="task-item">
+                        <span className={`task-name ${task.done ? 'done' : ''}`}>{task.text}</span>
+                        <div className="buttons-wrapper">
+                            <button data-testid={`toggle-btn-${index}`} className="toggle-btn" onClick={() => toggleTask(index)}>{`Oznacz jako ${task.done ? "nie" : ""}wykonane`}</button>
+                            <button data-testid={`delete-btn-${index}`} className="delete-btn" onClick={() => deleteTask(index)}>Usuń</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
